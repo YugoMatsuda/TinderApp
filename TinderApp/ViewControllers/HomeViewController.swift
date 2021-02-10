4605f3893617a74dd485138be09a45b140e167b3
Yugo Matsuda <yugo1997zazen@gmail.com>
2021-02-10 21:38:23 +0900

    CardViewレイアウト作成

--- TinderApp/ViewControllers/HomeViewController.swift
+++ TinderApp/ViewControllers/HomeViewController.swift
@@ -16,26 +16,26 @@ class HomeViewController: UIViewController {
           
           let topControllView = TopControllView()
           
-          let view2 = UIView()
-          view2.backgroundColor = .blue
+          let cardView = CardView()
           
           let bottomControllView = BottomControllView()
           
-          let stackView = UIStackView(arrangedSubviews: [topControllView, view2, bottomControllView])
-          stackView.translatesAutoresizingMaskIntoConstraints = false
-          stackView.axis = .vertical
-          
-          self.view.addSubview(stackView)
-          
-          [
-              topControllView.heightAnchor.constraint(equalToConstant: 100),
-              bottomControllView.heightAnchor.constraint(equalToConstant: 120),
-              
-              stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-              stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-              stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
-              stackView.rightAnchor.constraint(equalTo: view.rightAnchor)]
-              .forEach { $0.isActive = true }
+        let stackView = UIStackView(arrangedSubviews: [topControllView, cardView, bottomControllView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        self.view.addSubview(stackView)
+        
+        [
+            topControllView.heightAnchor.constraint(equalToConstant: 100),
+            bottomControllView.heightAnchor.constraint(equalToConstant: 120),
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)]
+            .forEach { $0.isActive = true }
+                
                   
           
           // Do any additional setup after loading the view.
